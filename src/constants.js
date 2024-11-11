0f85b5a6e136e698ad679625fec1fe17edaf22e7
meghasaji2002 <meghasaji2002@gmail.com>
2024-11-11 10:30:58 +0530

    apiURL replaced with deployed backend url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const apiURL= 'http://localhost:5000';
+export const apiURL= 'https://expense-tracker-node-backend.onrender.com';
 export const fetchHeaders = {
     "Content-Type":"application/json",
     "Authorization":"Bearer " + sessionStorage.getItem('EXPENSE-TRACKER-TOKEN')
